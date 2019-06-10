1d40aef79561c0648c151403d59dbebc268622bd
Kumar Priyansh <bobbyskhs@gmail.com>
2019-06-10 18:26:11 +0530

    Added Main JS and Starter HTML

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'src/index.html'),
     protocol: 'file:',
     slashes: true
   }))
