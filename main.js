81f6e04e2afe9505f9c97d1e3f25bd9347683d8c
Kumar Priyansh <bobbyskhs@gmail.com>
2019-06-11 16:06:58 +0530

    Added Main Window Styles, Updated Main JS

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    frame: false,
+    transparent: true
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -15,7 +20,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
