92339db9ca93839cbe1e4c7a23349fa7e9d3caf9
Kumar Priyansh <bobbyskhs@gmail.com>
2020-07-17 21:20:37 +0530

    Integrated NativeKit

--- src/scripts/starterWindow.js
+++ src/scripts/starterWindow.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-    $('.nav-group').on('click', 'span', function() {
+$(document).ready(function () {
+    $('.nav-group').on('click', 'span', function () {
         $('.nav-group span.active').removeClass('active');
         $(this).addClass('active');
     });
-    $('.templateIcon').click(function(e){
+    $('.templateIcon').click(function (e) {
         $('.template .templateIcon').removeClass('clicked');
         $('.template .templateName span').removeClass('clickedText');
         $(this).addClass('clicked');
         var parentId = ($(this).parent().attr('id'));
-        $('#'+parentId + ' .templateName span').addClass('clickedText')
+        $('#' + parentId + ' .templateName span').addClass('clickedText')
         e.stopPropagation()
     })
-    $(document).on("click", function(e) {
+    $(document).on("click", function (e) {
         if ($(e.target).is(".templateIcon, .templateName span") === false) {
             $('.template .templateIcon').removeClass('clicked');
             $('.template .templateName span').removeClass('clickedText');
         }
     });
-    $('#startSelected').click(function(){
-        if ($('.template .templateIcon').hasClass('clicked')){
+    $('#startSelected').click(function () {
+        if ($('.template .templateIcon').hasClass('clicked')) {
             var parentId = ($('.clicked').parent().attr('id'));
             start(parentId);
         }
